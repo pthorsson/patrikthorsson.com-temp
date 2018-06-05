0c1c097288cc0528cf2cc07b06befca0f4044d6c
pthorsson <p.thorsson@gmail.com>
2018-06-05 22:14:11 +0200

    Added content Updated script and style

--- app/script.js
+++ app/script.js
@@ -1,13 +1,39 @@
-window._page = {
+(function (w) {
+    var onReadyCallback = function() {},
+        public = {},
+        state = {
+            picture: false,
+        };
 
-    email: function(element) {
+    public.clickEmail = function(element) {
         var address = element.getAttribute('data-address'),
             site = element.getAttribute('data-site'),
-            href = 'mailto:' + address + '@' + site,
+            href = 'mailto:hello@' + site,
             link = document.createElement('a');
         
         link.href = href;
         link.click();
-    }
+    };
 
-};
+    public.setState = function(name, value) {
+        state[name] = value;
+
+        for (var key in state) {
+            if (state.hasOwnProperty(key) && !state[key]) return;
+        }
+
+        onReadyCallback();
+    };
+
+    public.ready = function(callback) {
+        onReadyCallback = callback;
+
+        public.setState('readyCallback', true);
+    };
+
+    w.page = public;
+})(window);
+
+page.ready(function() {
+    document.body.className = 'page-ready';
+});
